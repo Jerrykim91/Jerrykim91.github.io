28be71ee8aa3f5c2761b4b9472153b65e4000025
jerrykim91 <sun4131@gamil.com>
2022-03-27 22:04:04 +0900

    22.03.27__Btn Test

--- gitbook/modal.js
+++ gitbook/modal.js
@@ -5,6 +5,7 @@
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 const btnOpenPopup = document.querySelector('.btn-open-popup');
+// const btnOpenPopup_2 = document.getElementById('btn');
 
 btnOpenPopup.addEventListener('click', () => {
     modal.classList.toggle('show');
@@ -13,6 +14,15 @@ btnOpenPopup.addEventListener('click', () => {
         body.style.overflow = 'hidden';
     }
 });
+
+
+// btnOpenPopup_2.addEventListener('click', () => {
+//     modal.classList.toggle('show');
+
+//     if (modal.classList.contains('show')) {
+//         body.style.overflow = 'hidden';
+//     }
+// });
 
 modal.addEventListener('click', (event) => {
     if (event.target === modal) {
